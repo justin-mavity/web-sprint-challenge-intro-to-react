425e2c73feca93d0475aa26b622fed7bb0b82f60
justin-mavity <justinmavity1021@gmail.com>
2020-10-09 11:55:51 -0500

    foobar

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+import React from 'react'
+
+export default function Character({info, action}){
+    return (
+        <div className='characters'>
+        <div>{info.name}</div>
+        <button onClick={() => action(info.id)}>See details</button>
+        </div>
+    )
+}
